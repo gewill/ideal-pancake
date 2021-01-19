d8de0a87ba0384f304155535c04dea5818532e10
Ge Will <531sunlight@gmail.com>
2021-01-19 10:28:31 +0800

    Add Xcode project

---
+++ ideal-pancake/ideal-pancake/ideal_pancakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ideal_pancakeApp.swift
+//  ideal-pancake
+//
+//  Created by Will on 19/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct ideal_pancakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
